dd7249868927e0d28355c53e586ba14d40733b98
Rabi <rabinarayanpatra1999@gmail.com>
2025-01-23 23:53:21 +0530

    Auth: Complete clerk integration and nav creation.

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,18 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "../globals.css";
 import React from "react";
 import { Navbar } from "@/components/navbar";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Smart Todo",
@@ -25,13 +13,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Navbar />
-        {children}
-      </body>
-    </html>
+    <>
+      <Navbar />
+      {children}
+    </>
   );
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,3 +1,13 @@
+import { UserButton } from "@clerk/nextjs";
+import { Logo } from "@/components/logo";
+
 export const Navbar = () => {
-  return <nav>Navbar</nav>;
+  return (
+    <nav className={"p-4 flex justify-around shadow-md"}>
+      <div>
+        <Logo />
+      </div>
+      <UserButton />
+    </nav>
+  );
 };
